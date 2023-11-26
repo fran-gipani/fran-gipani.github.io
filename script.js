9492bda130f82f4d6cc8b2994eb47df4f2c4b89d
fran-gipani <152069039+fran-gipani@users.noreply.github.com>
2023-11-26 22:00:50 +0800

    initial commit
    Read.

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yay, see you on the 18th!";
+  question.innerHTML = "yes syg bauuuu";
   gif.src =
     "https://media.giphy.com/media/enrq327a3sMIJAS5jA/giphy.gif";
 });
